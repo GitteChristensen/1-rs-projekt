253ece3890ad5c4d021bff36f4b22ee0ea538d2b
GitteChristensen <119286746+GitteChristensen@users.noreply.github.com>
2023-05-30 14:52:55 +0200

    Add files via upload

---
+++ Spændingsdeler-1års.py
@@ -0,0 +1,16 @@
+from machine import Pin, ADC
+from time import sleep
+
+bat = ADC(Pin(36))
+bat.atten(ADC.ATTN_11DB)
+bat.width(ADC.WIDTH_12BIT)
+
+while True:
+   bat_val = bat.read()
+   m_spaending = bat_val/4095*3.3
+   print("Analog maalt vaerdi: ", m_spaending)
+   spaending = m_spaending * 5
+   print("Input spaending: ", spaending)
+   sleep(1)
+   
+   
